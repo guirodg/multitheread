b0edf3e2615c1569e38b22711152d74ab1e680de
Guilherme Rodrigues <guilherme.rodrigues@spcbrasil.org.br>
2022-09-02 09:48:13 -0300

    first commit

---
+++ src/main/java/com/multitheread/MultithereadApplication.java
@@ -0,0 +1,13 @@
+package com.multitheread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultithereadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultithereadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/multitheread/MultithereadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multitheread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultithereadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
